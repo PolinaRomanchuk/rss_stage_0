ce81c0161ac3b3a09e35417db20d4fd59995649e
polinar_99@mail.ru <polinar_99@mail.ru>
2024-08-16 20:25:49 +0300

    feat: done burger menu js

--- shelter/script.js
+++ shelter/script.js
@@ -1,8 +1,45 @@
 let burgerIcon = document.querySelector(".burger-menu");
 let burgerMenu = document.querySelector(".mobile-menu");
+let burgerMenuContainer = document.querySelector(".mobile-menu_container");
+let navigationLinks = document.querySelectorAll('.mobile-menu_navigation_link');
+let body = document.querySelector("body");
 
 
-function rotateIcon (){
+burgerIcon.onclick = function (e) {
+    toggleMenu();
+}
+
+navigationLinks.forEach(link => {
+    link.onclick = function (e) {
+        let linkPath = this.getAttribute('href');
+
+        if (linkPath != null && linkPath.startsWith('#')) {
+            e.preventDefault();
+            toggleMenu();
+            setTimeout(() => {
+                document.querySelector(linkPath).scrollIntoView({ behavior: 'smooth' });
+            }, 300);
+        }
+        else if (linkPath == null) {
+            toggleMenu();
+        }
+        else {
+            e.preventDefault();
+            toggleMenu();
+            setTimeout(() => {
+                window.location.href = linkPath;
+            }, 300);
+        }
+    }
+});
+
+burgerMenu.onclick = function (e) {
+    if (!e.target.closest('.mobile-menu_container')) { toggleMenu(); }
+}
+
+function toggleMenu() {
     burgerIcon.classList.toggle('clicked');
     burgerMenu.classList.toggle('clicked');
+    body.classList.toggle('clicked');
 }
+
