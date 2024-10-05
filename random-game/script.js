c271e983184e0013b74bc164e7af8842d1d58c46
PolinaRomanchuk <polinar_99@mail.ru>
2024-10-05 11:07:34 +0300

    feat: add basic stucture

---
+++ random-game/script.js
@@ -0,0 +1,43 @@
+const meme = [
+    {
+        id: "1",
+        img: "./assets/img/1.png"
+    },
+    {
+        id: "2",
+        img: "./assets/img/2.png"
+    },
+    {
+        id: "3",
+        img: "./assets/img/3.png"
+    },
+    {
+        id: "4",
+        img: "./assets/img/4.png"
+    },
+    {
+        id: "5",
+        img: "./assets/img/5.png"
+    },
+    {
+        id: "6",
+        img: "./assets/img/6.png"
+    },
+    {
+        id: "7",
+        img: "./assets/img/7.png"
+    },
+    {
+        id: "8",
+        img: "./assets/img/8.png"
+    },
+    {
+        id: "9",
+        img: "./assets/img/9.png"
+    },
+    {
+        id: "10",
+        img: "./assets/img/10.png"
+    }
+]
+
